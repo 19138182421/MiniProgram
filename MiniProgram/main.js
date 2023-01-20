495daab3da01ae498e040c4d155a4b02626adc67
杨亚威 <2241327575@qq.com>
2023-01-20 19:12:00 +0800

    暂时提交

--- MiniProgram/main.js
+++ MiniProgram/main.js
@@ -3,6 +3,35 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入网络请求包escook
+import { $http } from '@escook/request-miniprogram'
+//挂载$http到uni全局对象上
+uni.$http = $http
+//配置根路径
+$http.baseUrl = 'http://localhost:8080'
+
+//配置拦截器
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+//封装数据请求失败的弹框方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- MiniProgram/pages/home/home.vue
+++ MiniProgram/pages/home/home.vue
@@ -1,6 +1,26 @@
 <template>
 	<view>
-		Home信息
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<!-- 循环渲染轮播图的 item 项 -->
+			<swiper-item v-for="(item, i) in swiperList" :key="i">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?good_id=' + item.good_id">
+					<!-- 动态绑定图片的 src 属性 -->
+					<image :src="item.image_src"></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+
+		<!-- 分类导航区域 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item, i) in navList" :key="i">
+				<image  class="nav-img"></image>
+				<!-- :src="item.cat_imgsrc" -->
+				<view>
+					<text>{{item.cat_name}}</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +28,67 @@
 	export default {
 		data() {
 			return {
-				
-			};
-		}
+				// 1. 轮播图的数据列表，默认为空数组
+				swiperList: [],
+				// 分类导航的数据列表
+				navList: [],
+			}
+		},
+		onLoad() {
+			// 2. 在小程序页面刚加载的时候，调用获取轮播图数据的方法
+			this.getSwiperList()
+			// 调用获取分类数据的方法
+			this.getNavList()
+		},
+		methods: {
+			// 3. 获取轮播图数据的方法
+			async getSwiperList() {
+				// 3.1 发起请求
+				const {
+					data: res
+				} = await uni.$http.get('/getMessage')
+				// console.log(res)
+				// 3.2 请求失败
+				if (res.meta.status !== 200) {
+					return uni.$showMsg()
+				}
+				// 3.3 请求成功，为 data 中的数据赋值
+				this.swiperList = res.message
+			},
+			async getNavList() {
+				const {
+					data: res
+				} = await uni.$http.get('/getcategory/0')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// console.log(res)
+				this.navList = res.message
+			},
+		},
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.nav-list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0;
+		flex-wrap: wrap;
+		font-size: 30rpx;
+
+		.nav-img {
+			width: 60px;
+			height: 50px;
+			background-color: red;
+		}
+	}
 </style>
